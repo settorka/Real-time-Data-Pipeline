7c261eccfe0399bae37fa0e917c2fc472f2a26fd
Settor Amediku <amedikusettor@gmail.com>
2024-06-07 23:30:27 +0100

    ruby API service update

--- real-time-data-streaming/APIs/rails-service/kafkaConsumer.rb
+++ real-time-data-streaming/APIs/rails-service/kafkaConsumer.rb
@@ -4,7 +4,7 @@ require 'mongo'
 mongo_client = Mongo::Client.new(['mongo:27017'], database: 'mydatabase')
 collection = mongo_client[:generatedRecords]
 
-kafka = Kafka.new(seed_brokers: ["localhost:9092"], client_id: "rails-service")
+kafka = Kafka.new(seed_brokers: ["kafka:9092"], client_id: "rails-service")
 
 consumer = kafka.consumer(group_id: "rails-group")
 consumer.subscribe("rails")
